c817c631fbcef38f8284302ba52ceaec94163e36
KevGary <kevgary.dev@gmail.com>
2015-12-23 23:30:58 -0400

    set up db and api endpoints for db

--- express-chess/routes/index.js
+++ express-chess/routes/index.js
@@ -9,11 +9,8 @@ var conString = 'postgres://@localhost/chess_db';
 var bcrypt = require('bcrypt');
 
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
-
+//-----------USERS--------
+//get all
 router.get('/api/users', function(req, res, next) {
   pg.connect(conString, function(err, client, done) {
     if (err) {
@@ -102,10 +99,191 @@ router.delete('/api/users/:id', function(req, res, next) {
   });
 });
 
-// router.get('*', function(req, res, next) {
-//   res.sendFile('index.html', {
-//       root : __dirname + '/../public/'
-//   })
-// });
+//------GAMES-------
+//get all
+router.get('/api/games', function(req, res, next) {
+  pg.connect(conString, function(err, client, done) {
+    if (err) {
+      return console.error('error fetching client from pool', err);
+    }
+    console.log("connected to database");
+    client.query('SELECT * FROM games', function(err, result) {
+      done();
+      if (err) {
+        return console.error('error running query', err);
+      }
+      res.send(result);
+    });
+  });
+});
+//post user
+router.post('/api/games', function(req, res, next) {
+  pg.connect(conString, function(err, client, done) {
+    if (err) {
+      return console.error('error fetching client from pool', err);
+    }
+    console.log("connected to database");
+    bcrypt.hash(req.body.password, 8, function(err, hash) {
+      client.query('INSERT INTO games(moves, result) VALUES($1, $2) returning id', [null, null], function(err, result) {
+        done();
+        if(err) {
+          return console.error('error running query', err);
+        }
+        res.send(result);
+      });
+    });
+  });
+});
+//get one
+router.get('/api/games/:id', function(req, res, next) {
+  pg.connect(conString, function(err, client, done) {
+    if (err) {
+      return console.error('error fetching client from pool', err);
+    }
+    console.log("connected to database");
+    client.query('SELECT * FROM games WHERE id = $1', [req.params.id], function(err, result) {
+      done();
+      console.log(req.params.id)
+      if (err) {
+        return console.error('error running query', err);
+      }
+      res.send(result);
+    });
+  });
+});
+// update one
+router.put('/api/games/:id', function(req, res, next) {
+  pg.connect(conString, function(err, client, done) {
+    if (err) {
+      return console.error('error fetching client from pool', err);
+    }
+    console.log("connected to database");
+    //query for password (storedPW)
+    //compare with .compareSync(req.body.password, storedPW)
+    bcrypt.hash(req.body.password, 8, function(err, hash) {
+      client.query('UPDATE games SET moves = $2, result = $3  WHERE id = $1', [req.params.id, req.body.moves, req.body.result], function(err, result) {
+        done();
+        if (err) {
+          return console.error('error running query', err);
+        }
+        res.send(result);
+      });
+    });
+  });
+});
+//delete one
+router.delete('/api/games/:id', function(req, res, next) {
+  pg.connect(conString, function(err, client, done) {
+     console.log(conString)
+    if (err) {
+      return console.error('error fetching client from pool', err);
+    }
+    console.log("connected to database");
+    client.query('DELETE FROM games WHERE id = $1',[req.params.id], function(err, result) {
+      done();
+      if (err) {
+        return console.error('error running query', err);
+      }
+      res.send(result);
+    });
+  });
+});
+
+
+//----------MATCHES-----------
+//get one
+router.get('/api/matches', function(req, res, next) {
+  pg.connect(conString, function(err, client, done) {
+    if (err) {
+      return console.error('error fetching client from pool', err);
+    }
+    console.log("connected to database");
+    client.query('SELECT * FROM matches', function(err, result) {
+      done();
+      if (err) {
+        return console.error('error running query', err);
+      }
+      res.send(result);
+    });
+  });
+});
+//post user
+router.post('/api/matches', function(req, res, next) {
+  pg.connect(conString, function(err, client, done) {
+    if (err) {
+      return console.error('error fetching client from pool', err);
+    }
+    console.log("connected to database");
+    bcrypt.hash(req.body.password, 8, function(err, hash) {
+      client.query('INSERT INTO matches(w_user_id, b_user_id, game_id) VALUES($1, $2, $3) returning id', [req.body.w_user_id, req.body.b_user_id, req.body.game_id], function(err, result) {
+        done();
+        if(err) {
+          return console.error('error running query', err);
+        }
+        res.send(result);
+      });
+    });
+  });
+});
+//get one
+router.get('/api/matches/:id', function(req, res, next) {
+  pg.connect(conString, function(err, client, done) {
+    if (err) {
+      return console.error('error fetching client from pool', err);
+    }
+    console.log("connected to database");
+    client.query('SELECT * FROM matches WHERE id = $1', [req.params.id], function(err, result) {
+      done();
+      console.log(req.params.id)
+      if (err) {
+        return console.error('error running query', err);
+      }
+      res.send(result);
+    });
+  });
+});
+// update one
+router.put('/api/matches/:id', function(req, res, next) {
+  pg.connect(conString, function(err, client, done) {
+    if (err) {
+      return console.error('error fetching client from pool', err);
+    }
+    console.log("connected to database");
+    //query for password (storedPW)
+    //compare with .compareSync(req.body.password, storedPW)
+    bcrypt.hash(req.body.password, 8, function(err, hash) {
+      client.query('UPDATE matches SET w_user_id = $2, b_user_id = $3, game_id = $4  WHERE id = $1', [req.params.id, req.body.w_user_id, req.body.b_user_id, req.body.game_id], function(err, result) {
+        done();
+        if (err) {
+          return console.error('error running query', err);
+        }
+        res.send(result);
+      });
+    });
+  });
+});
+//delete one
+router.delete('/api/matches/:id', function(req, res, next) {
+  pg.connect(conString, function(err, client, done) {
+     console.log(conString)
+    if (err) {
+      return console.error('error fetching client from pool', err);
+    }
+    console.log("connected to database");
+    client.query('DELETE FROM matches WHERE id = $1',[req.params.id], function(err, result) {
+      done();
+      if (err) {
+        return console.error('error running query', err);
+      }
+      res.send(result);
+    });
+  });
+});
+
+router.get('*', function(req, res, next) {
+  res.sendFile('index.html', {
+      root : __dirname + '/../public/'
+  })
+});
 
 module.exports = router;
